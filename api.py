e2b955ac72b665b8b195e5d43f35565fab05dccf
Mapie-llifr <142422370+Mapie-llifr@users.noreply.github.com>
2023-09-25 15:57:33 +0200

    Update api.py
    idem

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ api.config["DEBUG"] = True
 
 
 MODEL = 'small_model_final.joblib'
-#DATA_URL = "https://eu.pythonanywhere.com/user/Mapiellifr/files/home/Mapiellifr/OC_P7/small_df_model_final.csv"
+#DATA_URL = "https://github.com/Mapie-llifr/OC_P7/blob/main/Docs_projet7/small_df_model_final.csv" #"https://eu.pythonanywhere.com/user/Mapiellifr/files/home/Mapiellifr/OC_P7/small_df_model_final.csv"
 MODEL_URL = "https://github.com/Mapie-llifr/OC_P7/blob/main/" + MODEL
 #MODEL = "pipeline_lightGBM_final.joblib"        #local
 DATA_URL = "./Docs_projet7/small_df_model_final.csv"  #local
